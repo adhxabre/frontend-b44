f2ac1c2c6a0516978f2cfc61056a6cd367a02a98
adhxabre <abeldustin06@gmail.com>
2023-02-20 13:27:48 +0700

    inline styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import { useState } from "react";
-
-const pdip =
-  "https://upload.wikimedia.org/wikipedia/min/thumb/0/09/LOGO-_PDIP.svg/212px-LOGO-_PDIP.svg.png";
-
-const ferdiSambo =
-  "https://www.kenhub.com/thumbor/bMa9OaR37bsBZ4T3cFyXl83_MHI=/fit-in/413x413/filters:fill(FFFFFF,true):watermark(/images/watermark_only_sm.png,0,0,0):watermark(/images/logo_url_sm.png,-10,-10,0):format(jpeg)/images/anatomy_term/skull/eEsfu70EOMx1TlBf5tYAiA_Go0bFvBvzClwSivuaiELg_head_01.png";
+const styles = {
+  form: {
+    margin: "16px 20% 0",
+  },
+  formGroup: {
+    marginBottom: "16px",
+  },
+  formLabel: {
+    marginBottom: "8px",
+    display: "inline-block",
+  },
+  formInput: {
+    display: "block",
+    width: "100%",
+    padding: ".375rem .75rem",
+    fontSize: "1rem",
+    lineHeight: 1.5,
+    color: "#212529",
+    backgroundColor: "#fff",
+    border: "1px solid #ccaacc",
+    borderRadius: ".25rem",
+  },
+};
 
 export default function App() {
-  const [data, setData] = useState("");
-
   return (
-    <>
-      <h1>{data}</h1>
-      {data === "Puan Maharani" ? <img src={pdip} /> : <img src={ferdiSambo} />}
-      <input type="text" onChange={(e) => setData(e.target.value)} />
-    </>
+    <form style={styles.form}>
+      <div style={styles.formGroup}>
+        <label htmlFor="username" style={styles.formLabel}>
+          Username
+        </label>
+        <input
+          type="text"
+          id="username"
+          placeholder="Input username"
+          style={styles.formInput}
+        />
+      </div>
+    </form>
   );
 }
