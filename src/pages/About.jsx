5b56fa4ac3fcab599ed81cdcea6b5cd644d94a86
adhxabre <abeldustin06@gmail.com>
2023-02-20 14:55:58 +0700

    use navigate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 export default function About() {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/sign-up");
+  };
+
   return (
     <>
       <div>
         <h1>Tentang Saya</h1>
         <p>Puan Maharani adalah seorang penjual pulau</p>
+
+        <button onClick={handleNavigate}>Sign Up</button>
+
+        <button onClick={() => navigate("/sign-up")}>Sign Up</button>
       </div>
     </>
   );
