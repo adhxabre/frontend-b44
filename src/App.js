a21e796a32090598711792918eef41b90c31d0fe
adhxabre <abeldustin06@gmail.com>
2023-02-20 12:01:03 +0700

    State

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-const ListArray = ["Jokowi Dodo", "Puan Maharani", "Megawati", "Soekarno"];
+import { useState } from "react";
 
 export default function App() {
+  let total = 0;
+  const [count, setCount] = useState(0);
+
+  function Add() {
+    if (count === 10) return;
+    setCount(count + 1);
+    console.log(count);
+  }
+
+  function Less() {
+    if (count === 0) return;
+    setCount(count - 1);
+    console.log(count);
+  }
+
   return (
     <>
-      <h1>Ini list bro!</h1>
+      <h1>{count}</h1>
 
-      <Props ListData="BMW" ListName="C80" />
-      <Props ListData="Lemberjember" ListName="Galardo" />
-      <Props ListData="Toyota" ListName="Avanza Hitam" />
-      <Props ListData="Ferdi" ListName="Sambo" />
-      <Props ListData="PDI-P" ListName={ListArray[0]} />
-      <Props
-        ListData="Puan"
-        ListName="Maharani"
-        JualPulau={true}
-        BerapaPulau={34}
-      />
-    </>
-  );
-}
-
-function Props(props) {
-  return (
-    <>
-      <h1>{props.ListData}</h1>
-      <h2>{props.ListName}</h2>
-      <h3>{props.JualPulau}</h3>
-      <h4>{props.BerapaPulau}</h4>
+      <button onClick={Add}>Add</button>
+      <button onClick={Less}>Less</button>
     </>
   );
 }
