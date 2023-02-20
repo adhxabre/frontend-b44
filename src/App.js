533105e4749f6c3e12c02816d3114809aae7f486
adhxabre <abeldustin06@gmail.com>
2023-02-20 12:10:38 +0700

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import { useState } from "react";
 
+const pdip =
+  "https://upload.wikimedia.org/wikipedia/min/thumb/0/09/LOGO-_PDIP.svg/212px-LOGO-_PDIP.svg.png";
+
+const ferdiSambo =
+  "https://www.kenhub.com/thumbor/bMa9OaR37bsBZ4T3cFyXl83_MHI=/fit-in/413x413/filters:fill(FFFFFF,true):watermark(/images/watermark_only_sm.png,0,0,0):watermark(/images/logo_url_sm.png,-10,-10,0):format(jpeg)/images/anatomy_term/skull/eEsfu70EOMx1TlBf5tYAiA_Go0bFvBvzClwSivuaiELg_head_01.png";
+
 export default function App() {
-  let total = 0;
-  const [count, setCount] = useState(0);
-
-  function Add() {
-    if (count === 10) return;
-    setCount(count + 1);
-    console.log(count);
-  }
-
-  function Less() {
-    if (count === 0) return;
-    setCount(count - 1);
-    console.log(count);
-  }
+  const [data, setData] = useState("");
 
   return (
     <>
-      <h1>{count}</h1>
-
-      <button onClick={Add}>Add</button>
-      <button onClick={Less}>Less</button>
+      <h1>{data}</h1>
+      {data === "Puan Maharani" ? <img src={pdip} /> : <img src={ferdiSambo} />}
+      <input type="text" onChange={(e) => setData(e.target.value)} />
     </>
   );
 }
