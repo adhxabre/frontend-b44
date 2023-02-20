14a306792c38073525bc73246019b63fc65bf6df
adhxabre <abeldustin06@gmail.com>
2023-02-20 14:09:22 +0700

    Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import cssModules from "./App.module.css";
 
+import { Form, Col, Row } from "react-bootstrap";
+
 export default function App() {
   return (
     <form className={cssModules.form}>
@@ -14,6 +16,17 @@ export default function App() {
           className={cssModules.formInput}
         />
       </div>
+      <div className="formGroup">
+        <label htmlFor="email" className="formLabel">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Input email"
+          className="formInput"
+        />
+      </div>
       <div className={cssModules.formGroup}>
         <label htmlFor="password" className={cssModules.formLabel}>
           Password
@@ -25,6 +38,17 @@ export default function App() {
           className={cssModules.formInput}
         />
       </div>
+
+      <Row>
+        <Form.Group as={Col} md="6" style={{ marginBottom: "16px" }}>
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" placeholder="Input city" required />
+        </Form.Group>
+        <Form.Group as={Col} md="6" style={{ marginBottom: "16px" }}>
+          <Form.Label>State</Form.Label>
+          <Form.Control type="text" placeholder="Input state" required />
+        </Form.Group>
+      </Row>
     </form>
   );
 }
