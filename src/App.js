d1a282c9dc622ebb5e3bcd3b4e7b2416dda6007c
adhxabre <abeldustin06@gmail.com>
2023-02-21 10:17:35 +0700

    state func

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import ExampleForm from "./pages/ExampleForm";
 
--- src/pages/ExampleForm.jsx
+++ src/pages/ExampleForm.jsx
@@ -1,76 +1,71 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-export default class ExampleForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fullname: "",
-      email: "",
-      password: "",
-    };
-  }
+export default function ExampleForm() {
+  const [form, setForm] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+  });
 
-  handleOnChange = (e) => {
-    this.setState({
-      ...this.state, // fullname, email, password
+  const handleOnChange = (e) => {
+    setForm({
+      ...form, // fullname, email, password
       [e.target.name]: e.target.value,
     });
   };
 
-  handleOnSubmit = (e) => {
+  const handleOnSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    console.log(form);
   };
 
-  render() {
-    return (
-      <Container>
-        <Row className="d-flex align-items-center justify-content-center vh-100">
-          <Col md="6">
-            <Form onSubmit={this.handleOnSubmit}>
-              <div className="text-center h3">REGISTER</div>
-              <Form.Group className="mb-3" controlId="formFullname">
-                <Form.Label>Full Name</Form.Label>
-                <Form.Control
-                  onChange={this.handleOnChange}
-                  value={this.state.fullname}
-                  name="fullname"
-                  size="sm"
-                  type="text"
-                  placeholder="Puan Maharani"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  onChange={this.handleOnChange}
-                  value={this.state.email}
-                  name="email"
-                  size="sm"
-                  type="email"
-                  placeholder="puanmaharani@pdip.id"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  onChange={this.handleOnChange}
-                  value={this.state.password}
-                  name="password"
-                  size="sm"
-                  type="password"
-                  placeholder="puanpulau!012"
-                />
-              </Form.Group>
+  return (
+    <Container>
+      <Row className="d-flex align-items-center justify-content-center vh-100">
+        <Col md="6">
+          <Form onSubmit={handleOnSubmit}>
+            <div className="text-center h3">REGISTER</div>
+            <Form.Group className="mb-3" controlId="formFullname">
+              <Form.Label>Full Name</Form.Label>
+              <Form.Control
+                onChange={handleOnChange}
+                value={form.fullname}
+                name="fullname"
+                size="sm"
+                type="text"
+                placeholder="Puan Maharani"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                onChange={handleOnChange}
+                value={form.email}
+                name="email"
+                size="sm"
+                type="email"
+                placeholder="puanmaharani@pdip.id"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                onChange={handleOnChange}
+                value={form.password}
+                name="password"
+                size="sm"
+                type="password"
+                placeholder="puanpulau!012"
+              />
+            </Form.Group>
 
-              <Button variant="primary" type="submit" size="sm">
-                Submit
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+            <Button variant="primary" type="submit" size="sm">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
